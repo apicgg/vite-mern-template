0df1f1223d2301fe4fbdc87976ae1601fa3db5ec
Anurag Pramanik <p.anurag@outlook.com>
2022-07-28 21:05:22 +0530

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'Enter your backend host with port',
+    },
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+})
