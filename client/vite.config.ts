30256f08a098a3e3551fe6eab33179a731340d5b
Anurag Pramanik <p.anurag@outlook.com>
2023-04-01 10:27:06 +0530

    Add port for vite dev server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
       "/api": "Enter your backend host with port",
     },
